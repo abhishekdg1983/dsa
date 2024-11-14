6ce2a0942e8101dbabb5c0bfbfa83078b1e081a3
abhishekdg1983 <151938550+abhishekdg1983@users.noreply.github.com>
2024-11-14 11:52:49 +0530

    Update prefixsum.py
    Prefix sum pattern Range Sum Query - Immutable (LeetCode #303)

--- prefixsum.py
+++ prefixsum.py
@@ -1,1 +1,18 @@
+class NumArray:
+# https://leetcode.com/problems/range-sum-query-immutable/solutions/5689337/prefix-sum-approach/
+    def __init__(self, nums: List[int]):
+        self.prefix_sum = []
+        current_sum = 0
+        for num in nums:
+            current_sum += num
+            self.prefix_sum.append(current_sum)
 
+    def sumRange(self, left: int, right: int) -> int:
+        prefix_sum = self.prefix_sum
+        prefix_sum_r = prefix_sum[right]
+
+        if left == 0:
+            prefix_sum_l = 0
+        else:
+            prefix_sum_l = prefix_sum[left - 1]
+        return prefix_sum_r - prefix_sum_l
