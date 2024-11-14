f22198911a23772a11b6a40ae66b8d0d6f32b695
abhishekdg1983 <151938550+abhishekdg1983@users.noreply.github.com>
2024-11-14 12:50:28 +0530

    Create cont-with-most-water.py

---
+++ cont-with-most-water.py
@@ -0,0 +1,19 @@
+#https://leetcode.com/problems/container-with-most-water/description/
+#Using two pointer to get width easily and improve time complexity from O(n2) to O(n).
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        max_area = 0
+        left = 0
+        right = len(height) - 1
+
+        while left < right:
+            max_area = max(max_area, (right - left) * min(height[left], height[right]))
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        
+        return max_area
